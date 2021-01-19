82aca3f26a6cf0a0b4f5d6386b9ff2e96bf90308
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-18 22:52:54 -0300

    First Commit

---
+++ src/main/java/com/iamgrodrigues/dsdeliveryapi/DsdeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.dsdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamgrodrigues/dsdeliveryapi/DsdeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.dsdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
