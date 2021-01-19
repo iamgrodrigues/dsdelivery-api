c03715dfccf3d343f4cb3c5e1b6ac7d8c0a270bb
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-19 12:50:58 -0300

    wip: Add GET/products method

--- src/main/java/com/iamgrodrigues/dsdeliveryapi/controllers/ProductController.java
+++ src/main/java/com/iamgrodrigues/dsdeliveryapi/controllers/ProductController.java
@@ -1,9 +1,14 @@
 package com.iamgrodrigues.dsdeliveryapi.controllers;
 
+import com.iamgrodrigues.dsdeliveryapi.dto.ProductDTO;
 import com.iamgrodrigues.dsdeliveryapi.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -12,5 +17,9 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> findAll() {
+        List<ProductDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/iamgrodrigues/dsdeliveryapi/repositories/ProductRepository.java
+++ src/main/java/com/iamgrodrigues/dsdeliveryapi/repositories/ProductRepository.java
@@ -3,5 +3,9 @@ package com.iamgrodrigues.dsdeliveryapi.repositories;
 import com.iamgrodrigues.dsdeliveryapi.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByOrderByNameAsc();
 }
