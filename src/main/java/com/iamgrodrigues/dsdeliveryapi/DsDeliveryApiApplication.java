a424865a90526c8d996c340eec0f1a97a182c5b1
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-19 23:58:37 -0300

    style: Rename main class

--- src/main/java/com/iamgrodrigues/dsdeliveryapi/DsDeliveryApiApplication.java
+++ src/main/java/com/iamgrodrigues/dsdeliveryapi/DsDeliveryApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsdeliveryApiApplication {
+public class DsDeliveryApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsdeliveryApiApplication.class, args);
+		SpringApplication.run(DsDeliveryApiApplication.class, args);
 	}
 
 }
